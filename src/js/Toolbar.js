9eda023f35aa370ee266bb39acbbb650e5ace774
Alan Rahlf <arahlf@gmail.com>
2011-07-11 20:09:37 -0500

    Misc. refactoring.

--- src/js/Toolbar.js
+++ src/js/Toolbar.js
@@ -1,12 +1,15 @@
 /**
  * @class GOL.Toolbar
  * @extends Ext.toolbar.Toolbar
- * @cfg {GOL.model.Grid} grid
- * 
- * A Toolbar used to interact with a {@link GOL.model.Grid}.
+ * @cfg {GOL.model.Grid} grid A Toolbar used to interact with a {@link GOL.model.Grid}.
  */
 Ext.define("GOL.Toolbar", {
     extend: "Ext.toolbar.Toolbar",
+    
+    /**
+     * @cfg {Number} millisPerIteration The number of milliseconds to wait between each generation when "playing". Defaults to 50.
+     */
+    millisPerIteration: 50,
     
     // button iconCls configurations
     iconClsBomb: "gol-icon-bomb",
@@ -15,15 +18,35 @@ Ext.define("GOL.Toolbar", {
     iconClsPause: "gol-icon-pause",
     iconClsNext: "gol-icon-next",
     
-    millisPerIteration: 50,
-    
     initComponent: function() {
         this.items = this.createItems();
         this.callParent();
     },
     
+    /**
+     * Triggers the next generation of Cells.
+     */
+    triggerNextGeneration: function() {
+        this.grid.nextGeneration();
+    },
+    
+    /**
+     * Returns whether or not the Toolbar is "playing".
+     * @return {Boolean}
+     */
+    isPlaying: function() {
+        return !!this.intervalId;
+    },
+    
     createItems: function() {
-        return [this.createIconButton(this.iconClsBomb, this.onBombClick), "-", this.createIconButton(this.iconClsRewind, this.onRewind), this.createIconButton(this.iconClsPlay, this.onPlayClick), this.createIconButton(this.iconClsNext, this.grid.nextGeneration), "-", {
+        return [
+            this.createIconButton(this.iconClsBomb, this.onBombClick),
+            "-",
+            this.createIconButton(this.iconClsRewind, this.onRewindClick),
+            this.createIconButton(this.iconClsPlay, this.onPlayClick),
+            this.createIconButton(this.iconClsNext, this.onNextClick),
+            "-",
+        {
             xtype: "golmenubutton",
             registry: GOL.cell.Registry,
             text: "Cell Type",
@@ -59,10 +82,9 @@ Ext.define("GOL.Toolbar", {
     },
     
     onPlayClick: function(button) {
-        if (!this.intervalId) {
+        if (!this.isPlaying()) {
             button.setIconCls(this.iconClsPause);
-            var fn = Ext.bind(this.triggerNextGeneration, this);
-            this.intervalId = setInterval(fn, this.millisPerIteration);
+            this.intervalId = setInterval(Ext.bind(this.triggerNextGeneration, this), this.millisPerIteration);
         } else {
             button.setIconCls(this.iconClsPlay);
             clearInterval(this.intervalId);
@@ -71,7 +93,7 @@ Ext.define("GOL.Toolbar", {
     },
     
     onNextClick: function() {
-        if (!this.intervalId) {
+        if (!this.isPlaying()) {
             this.triggerNextGeneration();
         }
     },
@@ -81,9 +103,5 @@ Ext.define("GOL.Toolbar", {
     },
     
     onCellTypeSelect: function(menuButton, register) {
-    },
-    
-    triggerNextGeneration: function() {
-        this.grid.nextGeneration();
     }
 });
